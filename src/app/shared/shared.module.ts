a8ffa73bdaf530df65a89d4daf74dc03454f7b05
shashank priyadarshi <shashank9163882019@gmail.com>
2023-01-24 23:57:52 +0530

    added new component for api down, new shared service in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UnderConstructionComponent } from './under-construction/under-construction.component';
 import { PageNotExistComponent } from './page-not-exist/page-not-exist.component';
+import { ApiNotWorkingComponent } from './api-not-working/api-not-working.component';
 
 @NgModule({
-  declarations: [UnderConstructionComponent, PageNotExistComponent],
+  declarations: [
+    UnderConstructionComponent,
+    PageNotExistComponent,
+    ApiNotWorkingComponent,
+  ],
   imports: [CommonModule],
   exports: [UnderConstructionComponent, PageNotExistComponent],
 })
