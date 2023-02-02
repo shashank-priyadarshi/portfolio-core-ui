1bdc8fc90bed30c3243e6c01fff35c2b9103db04
shashank priyadarshi <shashank9163882019@gmail.com>
2023-02-02 23:27:34 +0530

    corrected e2e test command for ci, corrected e2e test, added more e2e tests and test cases

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,16 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
-  })
-})
+describe('Portfolio landing pages content header test', () => {
+  it('Visits the home page', () => {
+    cy.visit('/');
+    cy.contains('articles');
+    cy.contains('todos');
+    cy.contains('GitHub Activity');
+    cy.contains('Projects');
+    cy.contains('Hobbies');
+  });
+  it('Visits the todos page', () => {
+    cy.visit('/todos');
+    cy.contains('articles');
+    cy.contains('todos');
+    cy.contains('TODO');
+  });
+});
